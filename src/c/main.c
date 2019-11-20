4dca0a7bc4129dea68aa0ec0e69dcc2a7257543c
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2019-11-20 09:35:25 +0530

    Update main.c

--- src/c/main.c
+++ src/c/main.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 
+struct driver
+{
+    char id[20];
+    char name[30];
+    int lat;
+    int lon;
+};
+
 int main (void)
 {
-    printf("Find Cab\n");
+    printf("Welcome \nFind Cab\n");
+    
+    int lat, lon;   //Input Variables
+    
+    //Prompt user for the location
+    printf("Enter your location(lat lon) : ");
+    scanf("%d %d", &lat, &lon); //Get location
+    
     return 0;
 }
