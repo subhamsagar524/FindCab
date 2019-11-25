57aed9e3dac78d51a67992a3297e7628296a743c
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2019-11-25 15:21:57 +0530

    Added Comments

--- src/c/main.c
+++ src/c/main.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<string.h>
+#include<stdio.h>    //Used for printf, scanf, getc, fgets, FILE, EOF
+#include<string.h>   //Used for strcpy, strlen
 #define FILE_NAME "input.txt"
 #define MAX_LINE 125
 #define MAX_DATA 4
@@ -7,10 +7,12 @@
 #define MAX_ID 20
 #define MAX_NAME 30
 
+//Function Prototypes Decalarations
 int checkFile();
 char* getdatabyline(int lineno);
 int intconvert(char *num);
 
+//Defining the driver data-type
 struct driver
 {
     char id[MAX_ID];
@@ -19,17 +21,19 @@ struct driver
     int lon;
 };
 
+//Main function-Execution of code starts from here
 int main (void)
 {
     printf("Welcome \nFind Cab\n");
     
     int lat, lon;   //Input Variables
     static int n;	//Processing Variables
-    int i, a, j, cnt;
+    int i, a, j, cnt;	//Processing Variables
 
     //Check the file and assign the number of drivers registered
     n = checkFile();
-    struct driver data[n];
+	
+    struct driver data[n];    //Storage for driver details
     printf("\nWe are having %d drivers with us.\n", n);
     
     //Fetch the raw data from the file
@@ -37,7 +41,7 @@ int main (void)
     for (i = 0; i <= n; i++)
 	strcpy(raw[i], getdatabyline(i));
     
-    //Store the data from the array to the driver data type
+    //Extract the data from the array and split it to word and store them in driver data type
     char str[MAX_LINE];
     char splitStrings[MAX_DATA][MAX_LINE];
     for (a = 1; a <= n; a++)
@@ -51,8 +55,8 @@ int main (void)
 	    if (str[i] == ' ' || str[i] == '\0')
 	    {
 	        splitStrings[cnt][j] = '\0';
-		cnt++;	//for next word
-		j = 0;	//for next word, init index 0
+		cnt++;	//For next word
+		j = 0;	//For next word, init index 0
 	    }
 	    else
 	    {
@@ -60,6 +64,7 @@ int main (void)
 		j++;
 	    }
 	}
+	//Storing the data in the driver storage unit
 	strcpy(data[a-1].id, splitStrings[0]);
 	strcpy(data[a-1].name, splitStrings[3]);
 	data[a-1].lat = intconvert(splitStrings[1]);
@@ -124,6 +129,8 @@ int intconvert(char *num)
     int result, i;
     result = 0;
     for (i = 0; num[i] != '\0'; i++)
-        result = (result * 10) + (num[i] - TOINT);
+        result = (result * 10) + (num[i] - TOINT);    //The ASCII value of digit starts from 48 Ex: "3" -> (int)51
     return result;
 }
+//Programmed by Subham Sagar Paira https://www.github.com/subhamsagar524/
+//Source : FIndCab https://www.github.com/subhamsagar524/FindCab
